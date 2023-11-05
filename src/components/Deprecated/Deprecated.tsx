5d25aab6fd3d133f07a3d2623201247c37f294b5
David Dias <david@lessonup.com>
2023-11-05 15:50:35 +0000

    Structural improvements 2

--- src/components/Deprecated/Deprecated.tsx
+++ src/components/Deprecated/Deprecated.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import styled from "@emotion/styled";
 import { colors } from "@Foundations/colors";
 
-interface DeprecatedProps {
-  children: string;
-}
 export const Deprecated: React.FC<React.PropsWithChildren> = ({ children }) => (
   <StyledDeprecatedSpan>{children}</StyledDeprecatedSpan>
 );
